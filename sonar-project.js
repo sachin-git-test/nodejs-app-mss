588c1081063b15d264f25e5b4c324c7df07e9265
sachinexplore <sachin.hs.explore@gmail.com>
2026-03-05 01:54:11 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,7 +1,7 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://13.232.9.230:9980/',
+  serverUrl: 'http://13.232.9.230:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
